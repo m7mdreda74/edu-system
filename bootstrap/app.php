63378605d7533f0848db9265d5a13ffa82c7d021
m7mdreda74 <m7mdreda74@gmail.com>
2026-07-22 23:50:43 +0200

    Trust Vercel proxy headers to fix Mixed Content HTTPS issues

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // Trust Vercel's proxy so Laravel generates HTTPS URLs
+        $middleware->trustProxies(at: '*');
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
